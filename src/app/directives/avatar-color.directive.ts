b760cbef17bdbd8b914970ad73539149caeb1f67
lmiletic <l.miletic@msgnetconomy.net>
2019-06-28 11:25:36 +0200

    Unsubscribe from avatarColorChanged on destroy

--- src/app/directives/avatar-color.directive.ts
+++ src/app/directives/avatar-color.directive.ts
@@ -1,16 +1,20 @@
-import {Directive, HostBinding, OnInit} from '@angular/core';
-import {Util} from '../util/util';
+import { Directive, HostBinding, OnInit, OnDestroy } from '@angular/core';
+import { Util } from '../util/util';
 
 @Directive({
     selector: '[appAvatarColor]'
 })
-export class AvatarColorDirective implements OnInit {
+export class AvatarColorDirective implements OnInit, OnDestroy {
 
     @HostBinding('style.color') color;
 
     ngOnInit(): void {
         this.color = Util.getAvatarColor();
         Util.avatarColorChanged.subscribe(() => this.color = Util.getAvatarColor());
+    }
+
+    ngOnDestroy(): void {
+        Util.avatarColorChanged.unsubscribe();
     }
 
 }
